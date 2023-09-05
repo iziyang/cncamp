c4e7d79dc467652194d1c32b903c43515bb48ea3
iziyang <iyuanzz@qq.com>
2023-09-05 16:14:12 +0800

    httpserver 更新

--- module8/httpserver_k8s.go
+++ module8/httpserver_k8s.go
@@ -9,10 +9,15 @@ import (
 	"os/signal"
 	"syscall"
 	"time"
+
+	"github.com/gorilla/mux"
+	"github.com/sirupsen/logrus"
 )
 
+var logger = logrus.New()
+
 func main() {
-	mux := http.NewServeMux()
+	r := mux.NewRouter()
 
 	// 创建日志文件
 	logFile, err := os.OpenFile("access.log", os.O_CREATE|os.O_WRONLY|os.O_APPEND, 0666)
@@ -22,53 +27,70 @@ func main() {
 	defer logFile.Close()
 
 	// 设置日志输出到文件
-	log.SetOutput(logFile)
+	logger.SetOutput(logFile)
 
-	mux.HandleFunc("/localhost/healthz", func(w http.ResponseWriter, r *http.Request) {
+	// 设置日志级别
+	logLevel := os.Getenv("LOG_LEVEL")
+	fmt.Printf("loglevel is:", logLevel)
+	if logLevel == "DEBUG" {
+		logger.SetLevel(logrus.DebugLevel)
+	} else {
+		logger.SetLevel(logrus.InfoLevel)
+	}
+
+	logger.Debug("Debug level logging started") // Debug log
+
+	r.HandleFunc("/localhost/healthz", func(w http.ResponseWriter, r *http.Request) {
 		w.WriteHeader(http.StatusOK)
+		logger.Debug("Begin health check, check url is /localhost/healthz")
+		logger.Info("Health check passed") // Info log
 	})
-
-	mux.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 		for name, values := range r.Header {
 			for _, value := range values {
 				w.Header().Add(name, value)
+				logger.Debug("Header name value is:", name, value)
 			}
+
 		}
+		logger.Info("Request header has been writed in response.")
 
 		version := os.Getenv("VERSION")
 		w.Header().Set("Version", version)
 
-		log.Printf("Client IP: %s, HTTP Status Code: %d\n", r.RemoteAddr, http.StatusOK)
+		logger.WithFields(logrus.Fields{
+			"ClientIP":       r.RemoteAddr,
+			"HTTPStatusCode": http.StatusOK,
+		}).Info("Request processed")
+
 		fmt.Fprintf(w, "Hello, World!")
 	})
 
-	server := &http.Server{
-		Addr:    ":8080",
-		Handler: mux,
+	srv := &http.Server{
+		Handler:      r,
+		Addr:         ":8080",
+		WriteTimeout: 15 * time.Second,
+		ReadTimeout:  15 * time.Second,
 	}
 
-	// 创建一个信号通道，用于接收终止信号
-	stop := make(chan os.Signal, 1)
-	signal.Notify(stop, syscall.SIGTERM, syscall.SIGINT)
-
+	// 优雅终止
+	idleConnsClosed := make(chan struct{})
 	go func() {
-		// 启动HTTP服务器
-		if err := server.ListenAndServe(); err != nil && err != http.ErrServerClosed {
-			log.Fatalf("HTTP server listen: %s\n", err)
+		sigint := make(chan os.Signal, 1)
+		signal.Notify(sigint, os.Interrupt, syscall.SIGTERM)
+		<-sigint
+
+		// 优雅关闭
+		if err := srv.Shutdown(context.Background()); err != nil {
+			logger.WithError(err).Error("Server shutdown failed")
 		}
+		close(idleConnsClosed)
 	}()
 
-	// 等待终止信号
-	<-stop
-
-	// 创建一个5秒的超时上下文，用于优雅关闭服务器
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-	defer cancel()
-
-	// 关闭HTTP服务器
-	if err := server.Shutdown(ctx); err != nil {
-		log.Fatalf("HTTP server shutdown: %s\n", err)
+	if err := srv.ListenAndServe(); err != http.ErrServerClosed {
+		logger.WithError(err).Error("Server closed unexpectedly")
+		os.Exit(1)
 	}
 
-	log.Println("HTTP server gracefully stopped")
+	<-idleConnsClosed
 }
